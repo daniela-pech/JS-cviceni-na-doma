a44f867cfa2c66c54786dc6880e81ecd27ccf3da
Daniela Pech <randakova.daniela@gmail.com>
2025-03-24 17:21:17 +0100

    vypracovana cviceni

--- script.js
+++ script.js
@@ -1,2 +1,37 @@
 // Příjem divadla - spočítejte příjem divadla ze vstupného.
-document.body.innerHTML += 5 + 2;
+// document.body.innerHTML += 5 + 2;
+/*
+const prijem = 12 * 174 * 15;
+document.body.innerHTML += 'Měsíční příjem divadla je ' + prijem + ' EUR.';
+const student = 0.65 * 12 * 174 * 15;
+const prijemStudent = student * 0.4 + prijem * 0.6;
+document.body.innerHTML += '<p></p>';
+document.body.innerHTML +=
+  'Měsíční příjem se studenty je ' + prijemStudent + ' EUR.';
+document.body.innerHTML += '<p></p>';
+
+//Zaokrouhlete číslo na desetiny, setiny a celé stovky.
+const x = 280.254485415;
+document.body.innerHTML = Math.round(x * 10) / 10;
+document.body.innerHTML += '<p></p>';
+document.body.innerHTML = Math.round(x * 100) / 100;
+document.body.innerHTML += '<p></p>';
+document.body.innerHTML = Math.round(x / 100) * 100;
+
+
+//Vymyslete funkci, která simuluje hod kostkou.
+document.body.innerHTML += Math.floor(Math.random() * 6 + 1);
+
+
+// Formátovač data - Vypište do stránky hezky zformátované datum.
+const den = Number(prompt('Zadej den (číslo):'));
+const mesic = Number(prompt('Zadej měsíc (číslo):'));
+const rok = Number(prompt('Zadej rok (číslo):'));
+document.body.innerHTML += den + '. ' + mesic + '. ' + rok;
+
+
+// Fahrnheit vs. Celsius - Vytvořte převodník stupňů Fahrenheita na stupně Celsia.
+const far = prompt('Zadejte stupně Fahrnheit!');
+const cels = (5 * (far - 32)) / 9;
+document.body.innerHTML += 'To se rovná ' + cels + ' stupňů Celsia.';
+*/
